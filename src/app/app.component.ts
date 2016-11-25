758cd088602b3c93a641b3e5439c3fc63632564b
Alex Lakatos <alex.lakatos.dev@gmail.com>
2016-11-25 19:45:01 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>
-    {{title}}
-  </h1>
+  <nav>
+    <div class="nav-wrapper deep-orange">
+      <a href="#" class="brand-logo center"><img src="https://github.com/ladyleet/bananajs/raw/master/src/assets/banana-logo.png"/></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a routerLink="/about">About Bananas</a></li>
+        <li><a routerLink="/bananas">Go Bananas</a></li>
+      </ul>
+    </div>
+  </nav>
+  <router-outlet></router-outlet>
   `,
   styles: []
 })
